cd49eeb357068a5474400ef550ce5e4213ddd992
Oladapo Daniel <oladapodaniel10@gmail.com>
2023-05-18 14:50:54 +0100

    cnfigured t allw crs

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ const cors = require('cors')
 
 // app.use(express.json())
 
-// app.use(cors({
-//     origin: '*'
-// }))
+app.use(cors({
+    origin: '*'
+}))
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
   
-//     if (req.method == "OPTIONS") {
-//       res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, PATCH, GET");
-//       return res.status(200).json({});
-//     }
+    if (req.method == "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, PATCH, GET");
+      return res.status(200).json({});
+    }
   
-//     next();
-//   });
+    next();
+  });
 
 // import { Server } from "socket.io";
 const { Server }  = require("socket.io")
